3ae0530c8475930facc6a53f0b1510876b57f6da
Liu Di <turbulence@live.cn>
2017-06-05 15:08:21 +0800

    取消对yiisoft/yii2-imagine的依赖

--- helpers/ResizeImage.php
+++ helpers/ResizeImage.php
@@ -3,7 +3,6 @@
 namespace yinxiaoshu\ueditor\helpers;
 
 use Yii;
-use yii\imagine\Image;
 use Imagine\Image\ImageInterface;
 use Imagine\Image\Box;
 use Imagine\Image\BoxInterface;
@@ -11,9 +10,77 @@ use Imagine\Image\Point;
 use GifFrameExtractor\GifFrameExtractor;
 use GifCreator\GifCreator;
 use yii\base\InvalidParamException;
+use yii\base\InvalidConfigException;
 
-class ResizeImage extends Image
+class ResizeImage
 {
+    /**
+     * GD2 driver definition for Imagine implementation using the GD library.
+     */
+    const DRIVER_GD2 = 'gd2';
+    /**
+     * imagick driver definition.
+     */
+    const DRIVER_IMAGICK = 'imagick';
+    /**
+     * gmagick driver definition.
+     */
+    const DRIVER_GMAGICK = 'gmagick';
+	
+    /**
+     * @var array|string the driver to use. This can be either a single driver name or an array of driver names.
+     * If the latter, the first available driver will be used.
+     */
+    public static $driver = [self::DRIVER_GMAGICK, self::DRIVER_IMAGICK, self::DRIVER_GD2];
+    /**
+     * @var ImagineInterface instance.
+     */
+    private static $_imagine;
+
+    /**
+     * Returns the `Imagine` object that supports various image manipulations.
+     * @return ImagineInterface the `Imagine` object
+     */
+    public static function getImagine()
+    {
+        if (self::$_imagine === null) {
+            self::$_imagine = static::createImagine();
+        }
+
+        return self::$_imagine;
+    }
+
+    /**
+     * Creates an `Imagine` object based on the specified [[driver]].
+     * @return ImagineInterface the new `Imagine` object
+     * @throws InvalidConfigException if [[driver]] is unknown or the system doesn't support any [[driver]].
+     */
+    protected static function createImagine()
+    {
+        foreach ((array) static::$driver as $driver) {
+            switch ($driver) {
+                case self::DRIVER_GMAGICK:
+                    if (class_exists('Gmagick', false)) {
+                        return new \Imagine\Gmagick\Imagine();
+                    }
+                    break;
+                case self::DRIVER_IMAGICK:
+                    if (class_exists('Imagick', false)) {
+                        return new \Imagine\Imagick\Imagine();
+                    }
+                    break;
+                case self::DRIVER_GD2:
+                    if (function_exists('gd_info')) {
+                        return new \Imagine\Gd\Imagine();
+                    }
+                    break;
+                default:
+                    throw new InvalidConfigException("Unknown driver: $driver");
+            }
+        }
+        throw new InvalidConfigException('Your system does not support any of these drivers: ' . implode(',', (array) static::$driver));
+    }	
+	
     protected static function getBox(BoxInterface $sourceBox, $width, $height, $keepAspectRatio = true)
     {
         if ($width === null && $height === null) {
