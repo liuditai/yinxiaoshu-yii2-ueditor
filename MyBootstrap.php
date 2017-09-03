b559da1299bb1d284d45d7bee69bd242179ea888
Liu Di <turbulence@live.cn>
2017-09-03 09:00:41 +0800

    使插件中的 module 控制住自己的 url 解析，以防 pretty url 时其他的 urlRule 造成的干扰

--- MyBootstrap.php
+++ MyBootstrap.php
@@ -14,11 +14,19 @@ class MyBootstrap implements BootstrapInterface
 			if (isset(Yii::$app->params['ueditor']['moduleName'])) {
 				$moduleName = Yii::$app->params['ueditor']['moduleName'];
 			}
-			Yii::$app->modules = array_merge(Yii::$app->modules,[
+			Yii::$app->modules = array_merge([
 				$moduleName => [
 					'class' => 'yinxiaoshu\ueditor\Module'
 				]
+			],Yii::$app->modules);
+			
+			// 使得以 $moduleName 打头的 url 都从这个模块中来处理
+			$rule = Yii::createObject([
+				'class' => 'yii\web\UrlRule',
+				'pattern' => $moduleName.'/<controller>/<action>',
+				'route' => $moduleName.'/<controller>/<action>',
 			]);
+			array_unshift(Yii::$app->urlManager->rules,$rule);
 		});
 	}
 }
